b145c4eb15d57874a2b23ece67f4a8be8ce31f84
AvutE <tuva@etavo.com>
2019-01-08 13:21:00 +0100

    Set up for team.html

--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var homeController = require('../controllers/home');
 
 router.get('/', homeController.Index);
+router.get('/team', homeController.Team)
 
 module.exports = router;
